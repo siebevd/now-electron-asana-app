a12d83447103208d752780008b7efc21925aab81
siebe van dijck <vandijckske@gmail.com>
2017-03-03 00:56:02 +0000

    started adding basics for redux

--- app/index.js
+++ app/index.js
@@ -1,31 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import { AppContainer } from 'react-hot-loader';
-// AppContainer is a necessary wrapper component for HMR
+import reducers from './reducers';
 
 import App from './components/App';
 
+
+const store = createStore(reducer);
+
+// AppContainer is a necessary wrapper component for HMR
 const render = (Component) => {
-  ReactDOM.render(
-    <AppContainer>
-      <Component/>
-    </AppContainer>,
-    document.getElementById('root')
-  );
+	ReactDOM.render(
+		<AppContainer>
+			<Provider>
+				<Component/>
+			</Provider>
+		</AppContainer>,
+		document.getElementById('root')
+	);
 };
 
 // document.onreadystatechange = function () {
-  // if (document.readyState == "complete") {
+	// if (document.readyState == "complete") {
 
 		render(App);
 
 		// Hot Module Replacement API
 		if (module.hot) {
-		  module.hot.accept('./components/App', () => {
-		    render(App)
-		  });
+			module.hot.accept('./components/App', () => {
+				render(App)
+			});
 		}
 
-  // }
+	// }
 // };
